e906178a322d7e7963cd202ebc03cef476c9e0bb
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2023-05-13 17:16:44 +0700

    Update Core

--- Assets/Game/Scripts/Managers/StorageManager.cs
+++ Assets/Game/Scripts/Managers/StorageManager.cs
@@ -7,13 +7,13 @@ namespace Game.Managers.StorageManager
 
 	public class Storage : StarSmithGames.Core.StorageSystem.Storage
 	{
-		public IStorageData<int> DeathCount { get; private set; }
+		public StorageData<int> DeathCount { get; private set; }
 
 		protected override void Initialization()
 		{
 			base.Initialization();
 
-			DeathCount = new StorageData<int>(Database, "death_count", 0);
+			DeathCount = new(Database, "death_count", 0);
 		}
 
 		protected override void Purge() { }
