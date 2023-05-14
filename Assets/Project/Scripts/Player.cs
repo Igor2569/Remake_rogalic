341c404295227e87ea86a96af501c9900c6987e2
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2023-05-14 11:55:25 +0700

    Squashed commit of the following:
    commit 58cd111d4ac23f0a13f3659784178480554a3457
    Author: topsed <krugov.06i@gmail.com>
    Date:   Sun May 14 00:06:30 2023 +0300
    
        koala1
    
    commit 3876f873d9cc9ffda6a51d7f1b7b81cc7fcea731
    Author: topsed <krugov.06i@gmail.com>
    Date:   Sat May 13 17:54:46 2023 +0300
    
        player

---
+++ Assets/Project/Scripts/Player.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Player : MonoBehaviour
+{
+    private float movex;
+    public Animator anim;
+    private float speed = 30f;
+    public Rigidbody2D rb;
+    private float movey;
+    private Vector2 moveVector;
+    private Action movement;
+    private float scalex;
+    private float scaley = 4.039235f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = Go;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.Invoke();
+    }
+    private void Go()
+    {
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+        moveVector.x = Input.GetAxis("Horizontal") * Time.deltaTime;
+        moveVector.y = Input.GetAxis("Vertical") * Time.deltaTime;
+        rb.MovePosition(rb.position + moveVector * speed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            scalex = 3.574679f;
+            transform.localScale = new Vector2(scalex, scaley);
+            anim.SetFloat("Horizontal", 0f);
+            anim.SetFloat("Vertical", 1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            scalex = 3.574679f;
+            transform.localScale = new Vector2(scalex, scaley);
+            anim.SetFloat("Horizontal", 0f);
+            anim.SetFloat("Vertical", -1f);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            scalex = 3.574679f;
+            transform.localScale = new Vector2(scalex, scaley);
+            anim.SetFloat("Horizontal", 1f);
+            anim.SetFloat("Vertical", 0f);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            scalex = -3.574679f;
+            transform.localScale = new Vector2(scalex, scaley);
+            anim.SetFloat("Horizontal", -1f);
+            anim.SetFloat("Vertical", 0f);
+        }
+    }
+}
