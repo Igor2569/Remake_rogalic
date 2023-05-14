73f1ad7eff864fdb5c8950ebf4b47cd5b70c7616
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2023-05-14 15:49:50 +0700

    Fix Animations, Fix Movement

--- Assets/Game/Scripts/Entities/Player.cs
+++ Assets/Game/Scripts/Entities/Player.cs
@@ -1,74 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 namespace Game.Entities
 {
-    public class Player : MonoBehaviour
+	public class Player : MonoBehaviour
     {
-        private float movex;
-        public Animator anim;
-        public float speed = 30f;
-        public Rigidbody2D rb;
-        private float movey;
-        private Vector2 moveVector;
-        private Action movement;
-        private float scalex;
-        private float scaley = 4.039235f;
-        // Start is called before the first frame update
-        void Start()
-        {
-            movement = Go;
-        }
+        public Animator animator;
+        public Rigidbody2D rigidBody;
+        [Space]
+		public float speed = 30f;
 
-        // Update is called once per frame
-        void Update()
+		private Vector2 movement;
+
+        private int horizontalHash;
+        private int verticalHash;
+
+		private void Start()
+		{
+			horizontalHash = Animator.StringToHash("Horizontal");
+			verticalHash = Animator.StringToHash("Vertical");
+		}
+
+		private void Update()
+		{
+            movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//Input
+
+			var scale = transform.localScale;
+			scale.x = movement.x >= 0 ? 1f : -1f;
+			transform.localScale = scale;
+
+			animator.SetFloat(horizontalHash, movement.x);
+			animator.SetFloat(verticalHash, movement.y);
+		}
+
+		private void FixedUpdate()
         {
-            movement.Invoke();
-        }
-        private void Go()
-        {
-            movex = Input.GetAxis("Horizontal");
-            movey = Input.GetAxis("Vertical");
-            moveVector.x = Input.GetAxis("Horizontal") * Time.deltaTime;
-            moveVector.y = Input.GetAxis("Vertical") * Time.deltaTime;
-            rb.MovePosition(rb.position + moveVector * speed);
-            if (movex==0f && movey == 0)
-            {
-                scalex = 3.574679f;
-                transform.localScale = new Vector2(scalex, scaley);
-                anim.SetFloat("Horizontal", 0f);
-                anim.SetFloat("Vertical", 0f);
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                scalex = 3.574679f;
-                transform.localScale = new Vector2(scalex, scaley);
-                anim.SetFloat("Horizontal", 0f);
-                anim.SetFloat("Vertical", 1f);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                scalex = 3.574679f;
-                transform.localScale = new Vector2(scalex, scaley);
-                anim.SetFloat("Horizontal", 0f);
-                anim.SetFloat("Vertical", -1f);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                scalex = 3.574679f;
-                transform.localScale = new Vector2(scalex, scaley);
-                anim.SetFloat("Horizontal", 1f);
-                anim.SetFloat("Vertical", 0f);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                scalex = -3.574679f;
-                transform.localScale = new Vector2(scalex, scaley);
-                anim.SetFloat("Horizontal", -1f);
-                anim.SetFloat("Vertical", 0f);
-            }
-        }
-    }
+			rigidBody.MovePosition(rigidBody.position + (movement * speed * Time.fixedDeltaTime));
+		}
+	}
 }
