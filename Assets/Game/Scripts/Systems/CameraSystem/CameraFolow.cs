f6b011015226837d1991a759fb080b336c4dbcf7
Davidovich Igor Alexandrovich <sir.funtik18@gmail.com>
2023-05-14 12:57:19 +0700

    Fixed Camera on Scene

--- Assets/Game/Scripts/Systems/CameraSystem/CameraFolow.cs
+++ Assets/Game/Scripts/Systems/CameraSystem/CameraFolow.cs
@@ -1,7 +1,4 @@
 using Sirenix.OdinInspector;
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Systems.CameraSystem
@@ -9,10 +6,11 @@ namespace Game.Systems.CameraSystem
 	public class CameraFolow : MonoBehaviour
 	{
 		public Camera camera;
-		[OnValueChanged("GoToFolow", includeChildren: true)]
+		[OnValueChanged("RefreshPosition")]
 		public Transform folow;
 		[Min(0)]
 		public float smoothing = 0.1f;
+		[OnValueChanged("RefreshPosition")]
 		public Vector3 offset;
 
 		private void FixedUpdate()
@@ -23,6 +21,12 @@ namespace Game.Systems.CameraSystem
 		private void GoToFolow()
 		{
 			transform.position = folow?.position ?? transform.position;
+		}
+
+		private void RefreshPosition()
+		{
+			if (folow == null) return;
+			transform.position = folow.position + offset;
 		}
 	}
 }
