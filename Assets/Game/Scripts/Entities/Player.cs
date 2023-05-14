8dce51b7f44e52e4749775d19f5b3814750e13cb
topsed <krugov.06i@gmail.com>
2023-05-14 18:29:15 +0300

    joystick

--- Assets/Game/Scripts/Entities/Player.cs
+++ Assets/Game/Scripts/Entities/Player.cs
@@ -6,14 +6,12 @@ namespace Game.Entities
     {
         public Animator animator;
         public Rigidbody2D rigidBody;
-        [Space]
-		public float speed = 30f;
-
+		public float speed = 5f;
 		private Vector2 movement;
-
+		public FixedJoystick joystick;
         private int horizontalHash;
         private int verticalHash;
-
+		
 		private void Start()
 		{
 			horizontalHash = Animator.StringToHash("Horizontal");
@@ -23,12 +21,13 @@ namespace Game.Entities
 		private void Update()
 		{
             movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));//Input
-
+			
 			var scale = transform.localScale;
 			scale.x = movement.x >= 0 ? 1f : -1f;
-			transform.localScale = scale;
 
-			animator.SetFloat(horizontalHash, movement.x);
+            
+            transform.localScale = scale;
+            animator.SetFloat(horizontalHash, movement.x);
 			animator.SetFloat(verticalHash, movement.y);
 		}
 
